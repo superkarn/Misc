34ff045d56339d3f4a630bba524ed275eff9c822
Karn Ratana <karn.ratana@gmail.com>
2017-03-23 23:20:55 -0400

    Add Chocolatey scripts

---
+++ chocolatey/_install-chocolatey.ps1
@@ -0,0 +1,2 @@
+# https://chocolatey.org/install
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
---
+++ chocolatey/developers-tools.ps1
@@ -0,0 +1,43 @@
+## Common Developer Tools
+
+
+# BareTail
+# real-time log file monitoring tool
+# http://www.baremetalsoft.com/baretail/
+choco install baretail
+
+# Notepad++
+# Text editor
+# https://notepad-plus-plus.org/
+choco install notepadplusplus 
+
+# grepwin
+# RegEx search and replace for Windows
+# http://stefanstools.sourceforge.net/grepWin.html
+choco install grepwin 
+
+# Git
+# Code Repository
+# https://git-scm.com/
+choco install git 
+
+# TortoiseGit
+# UI for Git
+# https://tortoisegit.org/
+choco install tortoisegit 
+
+# Redis
+# in-memory data structure store
+# https://redis.io/
+choco install redis-64
+
+# Redis Desktop Manager
+# Cross-platform open source Redis DB management tool
+# https://redisdesktop.com/
+choco install redis-desktop-manager 
+
+# MariaDB
+# Relational Database, drop-in replacement of MySQL
+# https://mariadb.org/
+choco install mariadb 
+
