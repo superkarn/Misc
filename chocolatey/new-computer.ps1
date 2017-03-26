0714919086c8ff37623556f6f4e8bdb54ab25f67
Karn Ratana <karn.ratana@gmail.com>
2017-03-25 23:30:09 -0400

    Add chocolatey/new-computer.ps1

--- chocolatey/new-computer.ps1
+++ chocolatey/new-computer.ps1
@@ -1,46 +1,26 @@
-## Common software for all new computers
-
-
-# Google Chrome
-# Google's web browser
-# https://www.google.com/chrome/browser/desktop/index.html
-choco install googlechrome 
-
+## Common software for new computers
 
 
 choco install 7zip 
-choco install avidemux
-choco install boxstarter
-choco install calibre 
 choco install ccleaner 
 choco install coretemp 
-choco install curl 
 choco install dropbox
-choco install fiddler4
-choco install filezilla 
-
-# Firefox
-# Mozilla web browser
-# https://www.mozilla.org/en-US/firefox/new/
 choco install firefox 
-
-# f.lux
-# Computer monitor lighting software
-# https://justgetflux.com/
+choco install googlechrome 
 choco install f.lux 
 choco install gpu-z
-choco install inkscape
 choco install keepass
-choco install nodejs 
+choco install notepadplusplus 
 choco install paint.net 
-
-# Postman
-# API Development Tool
-# https://www.getpostman.com/
-choco install postman 
-
-choco install putty 
 choco install steam 
-choco install sysinternals 
 choco install vlc 
-choco install windirstat 
+
+
+## Miscallaneous 
+
+
+#choco install avidemux
+#choco install boxstarter
+#choco install calibre 
+#choco install filezilla 
+#choco install inkscape
