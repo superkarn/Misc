a75ef69c45600000336050e947c926bb0d26b7ce
Karn Ratana <superkarn@gmail.com>
2017-09-23 21:25:47 -0400

    Create mariadb.ps1

---
+++ docker/mariadb.ps1
@@ -0,0 +1,13 @@
+# MariaDB Docker image
+# https://hub.docker.com/_/mariadb/
+#
+# How to start a new container
+#  - save database data to host folder
+#  - use conf file from host
+
+docker run \
+--name [my-container-name] \
+-v //d/Docker/volumes/[my-container-name]:/var/lib/mysql \
+-v //d/Docker/volumes/[my-container-name]/conf:/etc/mysql/conf.d \
+-e MYSQL_ROOT_PASSWORD=[my-root-password] \
+-d mariadb
