abc07ccfe93945d9284fbbaec08ae8a60f766275
Karn Ratana <superkarn@gmail.com>
2017-09-23 21:29:33 -0400

    Update mariadb.ps1

--- docker/mariadb.ps1
+++ docker/mariadb.ps1
@@ -5,9 +5,9 @@
 #  - save database data to host folder
 #  - use conf file from host
 
-docker run \
---name [my-container-name] \
--v //d/Docker/volumes/[my-container-name]:/var/lib/mysql \
--v //d/Docker/volumes/[my-container-name]/conf:/etc/mysql/conf.d \
--e MYSQL_ROOT_PASSWORD=[my-root-password] \
+docker run `
+--name [my-container-name] `
+-v //d/Docker/volumes/[my-container-name]:/var/lib/mysql `
+-v //d/Docker/volumes/[my-container-name]/conf:/etc/mysql/conf.d `
+-e MYSQL_ROOT_PASSWORD=[my-root-password] `
 -d mariadb
