bdca93c3eb796d5c7dffad9a03b4b60f738bb161
Karn Ratana <superkarn@gmail.com>
2018-05-07 22:14:10 -0400

    Update robocopy.ps1

--- powershell/robocopy.ps1
+++ powershell/robocopy.ps1
@@ -2,10 +2,14 @@
 $today = get-date
 $today_string = $today.ToString("yyyy-MM-dd")
 
-# Backs up the picture folder to drive G
+# Back up the picture folder to drive G
 $log_g = $today_string + "-Picture-backup-G.txt"
 robocopy "D:\SharedPictures\" "G:\Data\Backup\Pictures\" /mir /log:$log_g
 
-# Backs up the picture folder to drive F
+# Back up the picture folder to drive F
 $log_f = $today_string + "-Picture-backup-F.txt"
 robocopy "D:\SharedPictures\" "F:\Data\Backup\Pictures\" /mir /log:log_f
+
+# Back up the picture folder to AmazonPhotos folder, copying only jpgs
+$log_f = $today_string + "-AmazonPhotos-backup.txt"
+Robocopy "D:\SharedPictures\" "G:\Data\AmazonPhotos\" *.jpg /mir /log:log_f
