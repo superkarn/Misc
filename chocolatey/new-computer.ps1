c0e5b26249c6a1fa505f42a6ff47a704b0a05ae5
Karn Ratana <karn@ratana.info>
2022-02-06 09:07:32 -0500

    Update new-computer.ps1

--- chocolatey/new-computer.ps1
+++ chocolatey/new-computer.ps1
@@ -1,26 +1,13 @@
 ## Common software for new computers
 
 
-choco install 7zip 
-choco install ccleaner 
-choco install coretemp 
-choco install dropbox
-choco install firefox 
-choco install googlechrome 
-choco install f.lux 
-choco install gpu-z
-choco install keepass
-choco install notepadplusplus 
-choco install paint.net 
-choco install steam 
-choco install vlc 
-
-
-## Miscallaneous 
-
-
-#choco install avidemux
-#choco install boxstarter
-#choco install calibre 
-#choco install filezilla 
-#choco install inkscape
+choco install -y 7zip  
+choco install -y coretemp
+choco install -y f.lux 
+choco install -y firefox 
+choco install -y googlechrome 
+choco install -y gpu-z
+choco install -y keepass
+choco install -y notepadplusplus 
+choco install -y paint.net 
+choco install -y vlc
