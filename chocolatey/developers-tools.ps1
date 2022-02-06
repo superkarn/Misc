5ae725df9bbde89746c9df8a074ef9694c54e959
Karn Ratana <karn@ratana.info>
2022-02-06 09:04:53 -0500

    Update developers-tools.ps1

--- chocolatey/developers-tools.ps1
+++ chocolatey/developers-tools.ps1
@@ -1,84 +1,19 @@
 ## Common Developer Tools
 
-
-# BareTail
-# Real-time log file monitoring tool
-# http://www.baremetalsoft.com/baretail/
-choco install baretail
-
-# curl
-# Command line tool and library for transferring data with URLs
-# https://curl.haxx.se/
-choco install curl 
-
-# Fiddler
-# Web debugging proxy 
-# http://www.telerik.com/fiddler
-choco install fiddler4
-
-# Git
-# Code Repository
-# https://git-scm.com/
-choco install git 
-
-# grepwin
-# RegEx search and replace for Windows
-# http://stefanstools.sourceforge.net/grepWin.html
-choco install grepwin 
-
-# Node.js
-# JavaScript runtime
-# https://nodejs.org/en/
-choco install nodejs 
-
-# Notepad++
-# Text editor
-# https://notepad-plus-plus.org/
-choco install notepadplusplus 
-
-# posh-git
-# A set of PowerShell scripts which provide Git/PowerShell integration
-# https://github.com/dahlbyk/posh-git
-choco install poshgit 
-
-# Postman
-# API Development Tool
-# https://www.getpostman.com/
-choco install postman 
-
-# PuTTY
-# SSH and Telnet client 
-# http://www.chiark.greenend.org.uk/~sgtatham/putty/
-choco install putty 
-
-# Windows Sysinternals
-# Windows troubleshooting utilities
-# http://technet.microsoft.com/en-us/sysinternals/bb842062
-choco install sysinternals 
-
-# TortoiseGit
-# UI for Git
-# https://tortoisegit.org/
-choco install tortoisegit 
-
-# WinDirStat 
-# Disk usage statistics viewer and cleanup tool 
-# https://windirstat.net/
-choco install windirstat 
-
-
-
-########## 
-# OLD
-
-# Redis
-### Use Redis on Docker instead
-# In-memory data structure store
-# https://redis.io/
-#choco install redis-64
-
-# Redis Desktop Manager
-### Use Redis Commander on Docker instead
-# Cross-platform open source Redis DB management tool
-# https://redisdesktop.com/
-#choco install redis-desktop-manager 
+ choco install -y 7zip  
+#choco install -y avidemux
+ choco install -y azure-data-studio
+ choco install -y baretail
+ choco install -y firefox 
+ choco install -y git 
+ choco install -y googlechrome 
+ choco install -y grepwin 
+#choco install -y inkscape
+ choco install -y insomnia-rest-api-client
+ choco install -y notepadplusplus 
+#choco install -y paint.net 
+ choco install -y poshgit 
+#choco install -y sql-server-management-studio
+ choco install -y tortoisegit 
+#choco install -y winscp
+ choco install -y vscode
