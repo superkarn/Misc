628757f208f4ad9d0267755eef57493fdcd7910e
Karn Ratana <karn@ratana.info>
2024-01-03 01:23:46 -0500

    Update backup-pictures.ps1

--- powershell/backup-pictures.ps1
+++ powershell/backup-pictures.ps1
@@ -1,23 +1,30 @@
-# Create the log folder if not exist
-$log_path = "~\logs\backup\"
-
-if (!(test-path $log_path))
-{
-    New-Item -ItemType Directory -Force -Path $log_path 
-}
 
 # Get today's date
 $today = get-date
 $today_string = $today.ToString("yyyy-MM-dd")
 
 # Back up the picture folder to drive G
-$log_g = "$log_path$today_string-Picture-backup-G.txt"
-robocopy "D:\SharedPictures\" "G:\Data\Backup\Pictures\" /mir /log:$log_g
+$destination_path = "G:\Data\Backup"
+$log_filename = "$destination_path\$today_string-Picture-backup-G.txt"
+#echo "D:\SharedPictures\" "$destination_path\Pictures\" "/mir /log:$log_g"
+robocopy "D:\SharedPictures\" "$destination_path\Pictures\" /mir /log:$log_filename
+
+
 
 # Back up the picture folder to drive F
-$log_f = "$log_path$today_string-Picture-backup-F.txt"
-robocopy "D:\SharedPictures\" "F:\Data\Backup\Pictures\" /mir /log:$log_f
+$destination_path = "F:\Data\Backup"
+$log_filename = "$destination_path\$today_string-Picture-backup-F.txt"
+#echo "D:\SharedPictures\" "$destination_path\Pictures\" "/mir /log:$log_g"
+robocopy "D:\SharedPictures\" "$destination_path\Pictures\" /mir /log:$log_filename
+
+
+# Back up the picture folder to drive H
+$destination_path = "H:\Data\Backup"
+$log_filename = "$destination_path\$today_string-Picture-backup-H.txt"
+robocopy "D:\SharedPictures\" "$destination_path\Pictures\" /mir /log:$log_filename
+
 
 # Back up the picture folder to AmazonPhotos folder, copying only jpgs
-$log_amzn = "$log_path$today_string-AmazonPhotos-backup.txt"
-Robocopy "D:\SharedPictures\" "G:\Data\AmazonCloudDrive\Pictures\Backup" *.jpg /mir /log:$log_amzn
+$destination_path = "G:\Data\AmazonCloudDrive"
+$log_filename = "$destination_path\$today_string-AmazonPhotos-backup.txt"
+robocopy "D:\SharedPictures\" "$destination_path\Pictures\Backup" *.jpg /mir /log:$log_filename
