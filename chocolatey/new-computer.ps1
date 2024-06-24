f9b9b0f03027c993f12a70f1976c0eea53ee225a
Karn Ratana <karn@ratana.info>
2024-06-24 00:12:24 -0400

    Update new-computer.ps1
    Added obsidian and imdisk-toolkit

--- chocolatey/new-computer.ps1
+++ chocolatey/new-computer.ps1
@@ -7,7 +7,9 @@ choco install -y f.lux
 choco install -y firefox 
 choco install -y googlechrome 
 choco install -y gpu-z
+choco install -y imdisk-toolkit
 choco install -y keepass
 choco install -y notepadplusplus 
+choco install -y obsidian
 choco install -y paint.net 
 choco install -y vlc
